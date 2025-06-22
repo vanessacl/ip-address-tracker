57e955d8e3b6548e20e9efae3d1132e73c2403fc
Vanessa Castro <vanessacastrol16@gmail.com>
2025-06-22 13:10:04 -0700

    Remove unsed build files

--- build.js
+++ build.js
@@ -1,21 +1,20 @@
-// build.js
 const fs = require('fs')
 const dotenv = require('dotenv')
 const path = require('path')
 
-// Load .env file
 const env = dotenv.parse(fs.readFileSync(path.resolve(__dirname, '.env')))
-const apiKey = env.API_KEY
+const apiKey = env.API_KEY || process.env.API_KEY
 
-// Read script.js template
+if (!apiKey) {
+  console.error('Error: API key not found in .env or environment variables.')
+  process.exit(1)
+}
+
 let scriptContent = fs.readFileSync('js/script.js', 'utf8')
-
-// Inject API key as a global variable
 scriptContent = `window.env = { apiKey: "${apiKey}" };\n${scriptContent.replace(
-  'const { apiKey } = config;',
+  'const { apiKey } = window.env || {};',
   ''
 )}`
 
-// Write to a new file
 fs.writeFileSync('js/script.built.js', scriptContent, 'utf8')
 console.log('Build completed. Use js/script.built.js in index.html')
